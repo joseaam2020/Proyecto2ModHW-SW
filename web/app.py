bb74131912f7d1b1797533223b436f4b53cbefb8
joseaam2020 <joseaam2019@hotmail.com>
2026-05-03 08:31:08 -0600

    Primera version logica de ruta para crear y simular procesos

--- web/app.py
+++ web/app.py
@@ -1,5 +1,8 @@
 # web/app.py
 from flask import Flask, render_template, send_from_directory
+from flask import request, redirect, url_for
+from models.pipeline_builder import PipelineBuilder
+
 import os
 import json
 
@@ -19,6 +22,37 @@ def report(filename):
     with open(os.path.join(REPORTS_DIR, filename)) as f:
         report = json.load(f)
     return render_template('report.html', report=report, filename=filename)
+
+@app.route('/simulate', methods=['POST'])
+def simulate():
+    # Parse number of products and processes/tasks from the form
+    num_products = int(request.form['num_products'])
+    # Parse process/task structure
+    processes_data = []
+    i = 1
+    while True:
+        task_times = request.form.getlist(f'processes[{i}][tasks][]')
+        if not task_times:
+            break
+        processes_data.append({"tasks": [int(t) for t in task_times]})
+        i += 1
+
+    # Prepare product data (IDs 1..num_products)
+    products_data = list(range(1, num_products + 1))
+
+    # Build pipeline
+    builder = PipelineBuilder(processes_data, products_data)
+    processes = builder.build()
+
+    # Run simulation for a fixed number of ticks (e.g., 10)
+    NUM_TICKS = 10
+    for tick in range(1, NUM_TICKS + 1):
+        for process in processes:
+            process.tick()
+            report = process.generate_report()
+            process.save_report(report, tick)  # Save report for each process/tick if desired
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
